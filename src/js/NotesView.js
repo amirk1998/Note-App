1d63c87b33ac8c81dd368ee802c9ce012b63145d
amirk1998 <amirk1998.pv@gmail.com>
2023-01-19 12:23:28 +0330

    Active List Style has been updated:Part 01

--- src/js/NotesView.js
+++ src/js/NotesView.js
@@ -44,25 +44,22 @@ export default class NotesView {
     const Max_Body_Length = 50;
     const date = new Date(updated).toLocaleString('en', { dateStyle: 'full', timeStyle: 'short' });
     return `<!-- Item -->
-    <div class="notes-list-item border-b-[1px] border-solid border-white border-opacity-20 my-4 cursor-pointer text-white" data-note-id="${id}"> 
-    <div class="flex items-center justify-between " >
-    <div class="notes-small-title p-3 text-xl">${title}</div>
-        <span class="note-list-trash" data-note-id="${id}"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class=" w-5 h-5 stroke-red-500 hover:stroke-white ">
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-            />
-          </svg>
-        </span>
-      </div>  
-    
-      <div class="notes-small-body px-3 py-0">
-      ${body.substring(0, Max_Body_Length)}
-      ${body.length > Max_Body_Length ? '...' : ''}
+    <div class="notes-list-item clicked-item" data-note-id="${id}">
+        <div class="flex items-center justify-between">
+          <div class="notes-small-title p-3 text-xl">${title}</div>
+          <span class="note-list-trash" data-note-id="${id}"
+            ><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-5 h-5 stroke-red-500 hover:stroke-white">
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+              />
+            </svg>
+          </span>
+        </div>
+        <div class="notes-small-body px-3 py-0">${body.substring(0, Max_Body_Length)} ${body.length > Max_Body_Length ? '...' : ''}</div>
+        <div class="notes-small-updated p-3 text-neutral-400 italic text-left">${date}</div>
       </div>
-      <div class="notes-small-updated p-3 text-neutral-400 italic text-left">${date}</div>
-    </div>
     `;
   }
 
@@ -91,5 +88,16 @@ export default class NotesView {
         this.onNoteDelete(noteItem.dataset.noteId);
       });
     });
+  }
+
+  updateActiveNote(note) {
+    //
+    this.root.querySelector('.notes-title').value = note.title;
+    this.root.querySelector('.notes-body').value = note.body;
+
+    //remove selected classes
+
+    // add selected Class
+    this.root.querySelector(`.Not-clicked[data-note-id="${note.id}"]`).classList.add();
   }
 }
